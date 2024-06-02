d87abf393edf1ef7803d38d994d8e0dabd0f067b
Duong Huy Hoang <hoangodo@gmail.com>
2024-06-02 08:34:37 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import time
+from langchain_google_genai import ChatGoogleGenerativeAI
+import discord
+from discord.ext import tasks
+from dotenv import load_dotenv
+import os
+import aiohttp
+
+from models import BaseTextModel
+
+load_dotenv()
+os.environ['GOOGLE_API_KEY'] = os.getenv('GEMINI_API')
+TOKEN = os.getenv('DISCORD_TOKEN')
+#GUILD = os.getenv('DISCORD_GUILD')
+#intents = discord.Intents.all()
+
+discord_bot = discord.Bot(description="Omni chat bot")
+base_model = BaseTextModel(model_name="gemini-1.5-pro")
+# Replaced with nosql db later
+user_histories = {}
+
+@discord_bot.event
+async def on_ready():
+    guild = discord_bot.guilds[0]
+    print(
+        f'{discord_bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    keep_alive.start()
+
+@discord_bot.slash_command()
+async def general_chat(ctx, question):
+    await ctx.defer()
+
+    user_id = ctx.author.id
+    if user_id not in user_histories:
+        user_histories[user_id] = []
+
+    response, chat_history = base_model.invoke(question, user_histories[user_id])
+    user_histories[user_id] = chat_history
+
+    await ctx.respond(response)
+
+
+discord_bot.run(TOKEN)
---
+++ models.py
@@ -0,0 +1,121 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+from typing_extensions import override
+
+from utils import helpers
+
+load_dotenv()
+os.environ['GOOGLE_API_KEY'] = os.getenv('GEMINI_API')
+
+class BaseTextModel:
+    def __init__(self, model_name="gemini-1.5-pro", history_window = 20):
+        self.llm = ChatGoogleGenerativeAI(model=model_name)
+        self.history_window = history_window
+
+    def history_cut_off(self, chat_history):
+        '''
+            Cut off the 2 oldest messages (1 bot, 1 human)
+        '''
+        
+        return chat_history[2:] if len(chat_history) > self.history_window else chat_history
+
+    @staticmethod
+    def update_chat_history(chat_history, query, response):
+        chat_history.append(("human", query))
+        chat_history.append(("human", response))
+        return chat_history
+
+    def invoke(self, query, chat_history):
+        prompt = ChatPromptTemplate.from_template(
+            template = """You are an helpful AI assistant for Mr Hoang's community. Your job is to answer user question and you may need look through a chat history for references. \
+            Use creative tone with funny icons. 
+            Here is the chat history:
+            '''
+            {chat_history}
+            '''
+
+            Lastest user query:
+            '''
+            {query}
+            '''
+
+            Answer:
+            """
+        )
+
+        chain = prompt|self.llm|StrOutputParser()
+        response = chain.invoke({"chat_history": ChatPromptTemplate.from_messages(chat_history),
+                "query": query})
+        
+        chat_history = self.update_chat_history(chat_history, query, response)
+        chat_history = self.history_cut_off(chat_history)
+
+        return response, chat_history
+
+class BaseMultimodalModel(BaseTextModel):
+    def __init__(self, *args):
+        super(GenericMultimodalModel, self).__init__(*args)
+
+
+class TextModelWithRAG(BaseTextModel):
+    def __init__(self, *args):
+        super(TextModelWithRAG, self).__init__(*args)
+
+    def decide_reformulate_query(self, state):
+        chat_history = state["chat_history"]
+        query = state["query"]
+        reformulate_decider_prompt = ChatPromptTemplate.from_template(
+            template="""You are a decision maker. Given a chat history and the latest user query, you will check if that user query \
+            need to reference the chat history to be fully understood or not. Answer 'yes' if it need the chat history, otherwises, 'no'. \
+            Here is the chat history:
+            '''
+            {chat_history}
+            '''
+
+            Lastest user query:
+            '''
+            {query}
+            '''
+
+            """
+        )
+        
+        decider_chain = reformulate_decider_prompt|self.llm|StrOutputParser()
+        decision = decider_chain.invoke({"chat_history": chat_history, "query": query})
+        if not len(chat_history):
+            return "no"
+        else:
+            return decision
+
+    def reformulate_query(self, state):
+        chat_history = state["chat_history"]
+        query = state["query"]
+
+        reformulate_prompt = ChatPromptTemplate.from_template(
+            template="""You are a query rewriter. Given a chat history and the latest user query, you will reformulate the user query into a standalone query so that it \
+            can be understood without the chat history. Do not answer the user query.
+            Here is the chat history:
+            '''
+            {chat_history}
+            '''
+
+            Lastest user query:
+            '''
+            {query}
+            '''
+            """
+        )
+
+        reformulate_chain = reformulate_prompt|self.llm|StrOutputParser()
+        return {"query": reformulate_chain.invoke({"chat_history": chat_history, "query": query})}
+
+    def retriever(self, state):
+        pass
+    def generate(self, state):
+        pass
+    @override
+    def invoke(self, query, chat_history):
+        pass
